ff9d62ea77f5e761cf1e772898aa33410fbd2685
Yang Predator <newkidsth@gmail.com>
2024-03-10 21:53:36 +0700

    Able to retrieve profile_image in the scraper

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,6 +1,18 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-def get_github_profile(url):
+
+def get_github_profile(url = "https://github.com/SanfetCoder?tab=repositories"):
     html_bytes = urlopen(url).read()
     html = html_bytes.decode('utf-8')
+    # create a soup with Beautifulsoup using read html
+    soup = BeautifulSoup(html, 'html.parser')
+    # get profile image
+    images = soup.find_all('img')
+    for image in images:
+        if 'avatar' in image['class']:
+            profile_image = image['src']
+    print(profile_image)    
     return html
+
+get_github_profile()
