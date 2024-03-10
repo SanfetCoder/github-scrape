129ab4ff08a7f3c6130e73589d46aefb8b3b2308
Yang Predator <newkidsth@gmail.com>
2024-03-10 21:39:23 +0700

    Able to read githubLink

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return render_template('index.html')
+
+@app.route("/profile", methods=["POST"])
+def profile():
+    if request.method == "POST":
+        githubLink = request.form.get("githubLink")
+        return f'{githubLink}'
